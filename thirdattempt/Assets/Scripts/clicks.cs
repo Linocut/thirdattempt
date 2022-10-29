8e1ae600418227e40afd57da4d5fc123470a2933
Jasmine May Darman <65880499+Linocut@users.noreply.github.com>
2022-10-28 22:50:00 -0400

    door

---
+++ thirdattempt/Assets/Scripts/clicks.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class clicks : MonoBehaviour
+{
+    public Text idk;
+    // Start is called before the first frame update
+    void door()
+    {
+        idk.gameObject.SetActive(true);
+    }
+}
